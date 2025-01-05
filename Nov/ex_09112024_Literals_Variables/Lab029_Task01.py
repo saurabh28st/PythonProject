b252a339d7430a83d03660d26938f25dc6b285ec
saurabh28st <saurabhtw.st@gmail.com>
2025-01-05 14:50:31 +0530

    9th Nov -Part3

---
+++ Nov/ex_09112024_Literals_Variables/Lab029_Task01.py
@@ -0,0 +1,13 @@
+# Task for the today
+# take a 3 input from the user
+# perform the addition, subtraction, multiplication and division
+
+num1 = int(input("Please enter the input1 :"))
+num2 = int(input("Please enter the input2 :"))
+num3 = int(input("Please enter the input3 :"))
+
+add = num1+num2+num3
+print("Addition is :",add)
+
+
+
---
+++ Nov/ex_09112024_Literals_Variables/Lab030_Task02.py
@@ -0,0 +1,11 @@
+# Take 2 numbers from the user and print the Quotient and Reminder
+
+num1 = int(input("Please enter the input1 :"))
+num2 = int(input("Please enter the input2 :"))
+
+quo = num1/num2
+# print(quo)
+print("Quotient is :",int(quo))
+
+rem = num1%num2
+print("Reminder is :",rem)
