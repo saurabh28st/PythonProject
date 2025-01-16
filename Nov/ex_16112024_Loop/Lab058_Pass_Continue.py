5ae783c43ac5a5d2acd5fc54cd3ce4d13b33428a
saurabh28st <saurabhtw.st@gmail.com>
2025-01-16 15:50:49 +0530

    16th Nov

---
+++ Nov/ex_16112024_Loop/Lab058_Pass_Continue.py
@@ -0,0 +1,15 @@
+# for i in range(101):    # 0 to 100
+#     if i%2==0:
+#         print(i)
+
+for i in range(10):         # Pass (Pass will execute the code)
+    if i == 7:
+        pass
+        print(i)
+
+print("---------------")
+
+for i in range(10):         # Continue (continue will not execute his code)
+    if i == 7:
+        continue
+        print(i)
--- Nov/ex_16112024_Loop/Lab058_Q2.py
+++
@@ -1,3 +0,0 @@
-for i in range(101):    # 0 to 100
-    if i%2==0:
-        print(i)
